d77e3e1c510eb576ddaabd413cc6defdc8c422ce
TaielsinH <taielstuder@gmail.com>
2025-11-12 18:02:08 -0300

    correccion de connection string en docker compose

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a695b10f179599babbb026469a39e3358410dee1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94fd529debe469de4716c9281b87806083d96805")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
