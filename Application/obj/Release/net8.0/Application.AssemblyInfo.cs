94fd529debe469de4716c9281b87806083d96805
TaielsinH <taielstuder@gmail.com>
2025-11-12 17:32:29 -0300

    algunos cambios

--- Application/obj/Release/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Release/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a41a56203391c769844aebf75ae61237b7a93a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a695b10f179599babbb026469a39e3358410dee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
