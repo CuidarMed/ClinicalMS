95380c63253b0b66f63c48901c7e431783a7f5db
TaielsinH <taielstuder@gmail.com>
2025-11-13 12:37:06 -0300

    Dockerizado

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a695b10f179599babbb026469a39e3358410dee1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd011393fa39ca0d4f9303ecc198c0792ade4b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ClinicalMS/obj/Release/net8.0/ClinicalMS.AssemblyInfo.cs
+++ ClinicalMS/obj/Release/net8.0/ClinicalMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicalMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a41a56203391c769844aebf75ae61237b7a93a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd011393fa39ca0d4f9303ecc198c0792ade4b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicalMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicalMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
